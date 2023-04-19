751fa1eeaf8ad6b8981c9df31dac82c0e4683f26
asmerom yohannes <ayohannes@kabbee.org>
2023-04-19 13:15:03 +0300

    product_page

--- lib/product_page/product.dart
+++ lib/product_page/product.dart
@@ -1,21 +1,67 @@
+import 'package:ecommerce_dashboard/constants/constants.dart';
+import 'package:ecommerce_dashboard/product_page/products_ctrl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../widget/error_widget.dart';
+import 'widget/product_widget.dart';
 
-class Productpage extends ConsumerWidget {
+class Productpage extends ConsumerStatefulWidget {
   const Productpage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ProductpageState();
+}
+
+class _ProductpageState extends ConsumerState<Productpage> {
+  final User users = FirebaseAuth.instance.currentUser!;
+  @override
+  Widget build(BuildContext context) {
+    final products = ref.watch(productsprovider);
+
     return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Center(
-        child: Container(
-          child: Text(
-            'product page',
-            style: TextStyle(color: Colors.white),
+        padding: const EdgeInsets.all(16),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: secondaryColor,
+            title: const Text('product'),
+            actions: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Text(
+                    '${users.email}',
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+              )
+            ],
           ),
-        ),
-      ),
-    );
+          body: products.when(
+            data: (data) {
+              return PageView.builder(
+                itemCount: data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const addproductbutton(),
+                        const SizedBox(height: defaultPadding),
+                        for (var prod in data) productwidget(prod: prod),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            error: (Object error, StackTrace stackTrace) {
+              return adminErrorWidget(message: error.toString());
+            },
+            loading: () {
+              return const LoadingWidget();
+            },
+          ),
+        ));
   }
 }
---
+++ lib/product_page/widget/product_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/constants.dart';
+import '../../models/product.dart';
+import '../../widget/image_widget.dart';
+import '../add_product.dart';
+
+class productwidget extends StatelessWidget {
+  const productwidget({
+    super.key,
+    required this.prod,
+  });
+
+  final Product prod;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      margin: EdgeInsets.all(8),
+      color: secondaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Imagewidget(
+            imageurl: prod.imageURL,
+          ),
+          const SizedBox(height: xsPadding),
+          desctext(name: prod.name!),
+          desctext(name: prod.price!),
+        ],
+      ),
+    );
+  }
+}
+
+class addproductbutton extends StatelessWidget {
+  const addproductbutton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Addproduct()));
+        },
+        child: Text('add product', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class desctext extends StatelessWidget {
+  const desctext({
+    super.key,
+    required this.name,
+  });
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(name, style: TextStyle(fontSize: 16))),
+    );
+  }
+}
